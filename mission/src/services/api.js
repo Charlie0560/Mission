a3c7e6d8ad7df771a4ee84a2d5a8561316c76627
Charlie0560 <chaitanyalokhande87@gmail.com>
2025-05-25 00:20:40 +0530

    updated api url

--- mission/src/services/api.js
+++ mission/src/services/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API = axios.create({ baseURL: "https://mission-beta.vercel.app" });
+const API = axios.create({ baseURL: "https://mission-c2f5ipc4w-chaitanyalokhande87gmailcoms-projects.vercel.app" });
 
 export const processStocks = (data) => API.post('/api/process-stocks', data);
 export const getTaskStatus = (taskId) => API.get(`/api/task-status/${taskId}`);
