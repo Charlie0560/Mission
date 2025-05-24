d56db528e7968237a8004a841ec0113e2d2c7965
Charlie0560 <chaitanyalokhande87@gmail.com>
2025-05-25 00:16:45 +0530

    api url

--- mission/src/services/api.js
+++ mission/src/services/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API = axios.create({ baseURL: "http://127.0.0.1:5000" });
+const API = axios.create({ baseURL: "https://mission-beta.vercel.app" });
 
 export const processStocks = (data) => API.post('/api/process-stocks', data);
 export const getTaskStatus = (taskId) => API.get(`/api/task-status/${taskId}`);
